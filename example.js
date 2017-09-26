e12812907c3be64ba81198cff2205e2b34c2ae0e
Dmitrii Bondarchuk <dmitrii.bondarchuk@iii.com>
2017-09-26 18:08:56 +0300

    Provided example and library relized

---
+++ example.js
@@ -0,0 +1,7 @@
+const SubnetDiscoverer = require('./');
+
+const subnetDiscovere = new SubnetDiscoverer();
+subnetDiscovere.on('host:printer', (ip, deviceName) => {
+  console.log('printer', ip, deviceName);
+});
+subnetDiscovere.discover();
---
+++ index.js
@@ -0,0 +1,30 @@
+const SubnetsPinger = require('ping-subnet');
+const snmp = require('net-snmp');
+
+class SubnetDiscoverer extends SubnetsPinger {
+  constructor() {
+    super();
+  }
+
+  discover() {
+    this.ping();
+
+    this.on('host:alive', ip => {
+      const session = snmp.createSession(ip, 'public');
+      const oids = [
+        '1.3.6.1.2.1.25.3.2.1.2.1',
+        '1.3.6.1.2.1.43.11.1.1.6.1.1',
+        '1.3.6.1.2.1.25.3.2.1.3.1'
+      ];
+      let varBinds = [];
+
+      session.get(oids, (error, varbinds) => {
+        if (!error && varbinds.length == oids.length && varbinds[0].value == '1.3.6.1.2.1.25.3.1.5') {
+          this.emit('host:printer', ip, varbinds[2].value.toString());
+        }
+      });
+    });
+  }
+}
+
+module.exports = SubnetDiscoverer;
