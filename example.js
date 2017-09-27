f1145ea400e4567dff208fd7fa4f17dd8d4607b2
Dmitrii Bondarchuk <dmitrii.bondarchuk@iii.com>
2017-09-27 10:55:05 +0300

    Added event for snmp-tree

--- example.js
+++ example.js
@@ -1,7 +1,12 @@
 const SubnetDiscoverer = require('./');
 
 const subnetDiscovere = new SubnetDiscoverer();
+
 subnetDiscovere.on('host:printer', (ip, deviceName) => {
   console.log('printer', ip, deviceName);
+});
+
+subnetDiscovere.on('host:snmp', (ip, tree) => {
+  console.log('printer', ip, tree);
 });
 subnetDiscovere.discover();
