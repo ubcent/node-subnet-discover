8f1b467db4d10156da8630e274c542eb3147942c
Dmitrii Bondarchuk <dmitrii.bondarchuk@iii.com>
2017-11-07 14:32:24 +0300

    Increment version

--- example.js
+++ example.js
@@ -5,8 +5,4 @@ const subnetDiscovere = new SubnetDiscoverer();
 subnetDiscovere.on('host:printer', (ip, deviceName) => {
   console.log('printer', ip, deviceName);
 });
-
-subnetDiscovere.on('host:snmp', (ip, tree) => {
-  console.log('printer', ip, tree);
-});
 subnetDiscovere.discover();
--- index.js
+++ index.js
@@ -21,15 +21,6 @@ class SubnetDiscoverer extends SubnetsPinger {
       session.get(oids, (error, varbinds) => {
         if (!error && varbinds.length == oids.length && varbinds[0].value == '1.3.6.1.2.1.25.3.1.5') {
           this.emit('host:printer', ip, varbinds[2].value.toString());
-          let vBinds = [];
-          
-          session.walk('1.3.6.1', 20, varbinds => {
-            vBinds = vBinds.concat(varbinds);
-          }, error => {
-            if (!error) {
-              this.emit('host:snmp', ip, vBinds);
-            }
-          });
         }
       });
     });
