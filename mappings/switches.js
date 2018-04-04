10581058999bcc6a2c13dff8f98bc80d11628de0
jammmes <glazov.it@gmail.com>
2018-04-04 21:28:55 +0400

    change map-files to modules

--- mappings/switches.js
+++ mappings/switches.js
@@ -1,17 +1,17 @@
-var switches = [{
-    oid: "1.3.6.1.2.1.17.1.1",
-    name: "dot1dBaseBridgeAddress",
-    descr: "The MAC address used by this bridge when it must be referred to in a unique fashion "
+module.exports = [{
+  oid: "1.3.6.1.2.1.17.1.1",
+  name: "dot1dBaseBridgeAddress",
+  descr: "The MAC address used by this bridge when it must be referred to in a unique fashion "
 }, {
-    oid: "1.3.6.1.2.1.17.1.4",
-    name: "dot1dBasePortTable",
-    descr: "A table that contains generic information about every port that is associated with this bridge "
+  oid: "1.3.6.1.2.1.17.1.4",
+  name: "dot1dBasePortTable",
+  descr: "A table that contains generic information about every port that is associated with this bridge "
 }, {
-    oid: "1.3.6.1.2.1.17.4.3",
-    name: "dot1dTpFdbTable",
-    descr: "A table that contains information about unicast entries for which the bridge has forwarding and / or filtering information "
+  oid: "1.3.6.1.2.1.17.4.3",
+  name: "dot1dTpFdbTable",
+  descr: "A table that contains information about unicast entries for which the bridge has forwarding and / or filtering information "
 }, {
-    oid: "1.3.6.1.2.1.17.2.15",
-    name: "dot1dStpPortTable",
-    descr: "A table that contains port-specific information for the Spanning Tree Protocol "
-}]
+  oid: "1.3.6.1.2.1.17.2.15",
+  name: "dot1dStpPortTable",
+  descr: "A table that contains port-specific information for the Spanning Tree Protocol "
+}];
